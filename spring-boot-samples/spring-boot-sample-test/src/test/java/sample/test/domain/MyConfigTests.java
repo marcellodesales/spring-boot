be8a8381cf95d7b1d42f097db4ff5ad38d425fff
Marcello de Sales <Marcello_DeSales@intuit.com>
2016-05-10 15:42:54 -0700

    Auto-Configure Tests: Adding MyConfig to load the application.properties using Prefix
    This setup current does not work, although the documentation at
    https://spring.io/blog/2016/04/15/testing-improvements-in-spring-boot-1-4 says it
    should be working. The test case fails with the following message:
    
    Results :
    
    Failed tests:
    
      MyConfigTests.configLoaded:43 expected:<"jdbc:mysql://localhost/doesnotexist"> but was:<null>
    
    Tests run: 32, Failures: 1, Errors: 0, Skipped: 0
    
    So, there's at least an instance created, but the POJO is not properly setup with the
    property "url".
    
    *	new file:   src/main/java/sample/test/domain/MyConfig.java
    - POJO that loads the configuration using the prefix
    
    *	new file:   src/test/java/sample/test/domain/MyConfigTests.java
    - Test class that verifies if the property is properly loaded

---
+++ spring-boot-samples/spring-boot-sample-test/src/test/java/sample/test/domain/MyConfigTests.java
@@ -0,0 +1,46 @@
+/*
+MyConfig * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.test.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link UserRepository}.
+ *
+ * @author Phillip Webb
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { MyConfig.class })
+public class MyConfigTests {
+
+	@Autowired
+	private MyConfig config;
+
+	@Test
+	public void configLoaded() throws Exception {
+		assertThat(this.config).isNotNull();
+		assertThat(this.config.getUrl()).isEqualTo("jdbc:mysql://localhost/doesnotexist");
+	}
+
+}
