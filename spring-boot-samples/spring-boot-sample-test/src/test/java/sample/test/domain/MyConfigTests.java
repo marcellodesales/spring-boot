deb930fd8bf34b48fa8499d9dd115678ae8ddbff
Marcello de Sales <Marcello_DeSales@intuit.com>
2016-05-12 18:13:06 -0700

    Fixing the test by adding @EnableConfigurationProperties
    This fixes the test case and properly enables the test to run
    
    Results :
    
    Tests run: 32, Failures: 0, Errors: 0, Skipped: 0
    
    [INFO] ------------------------------------------------------------------------
    [INFO] BUILD SUCCESS
    [INFO] ------------------------------------------------------------------------
    [INFO] Total time: 10.578 s
    [INFO] Finished at: 2016-05-12T18:12:46-07:00
    [INFO] Final Memory: 25M/189M
    [INFO] ------------------------------------------------------------------------
    
    Running from Eclipse I couldn't run this.

--- spring-boot-samples/spring-boot-sample-test/src/test/java/sample/test/domain/MyConfigTests.java
+++ spring-boot-samples/spring-boot-sample-test/src/test/java/sample/test/domain/MyConfigTests.java
@@ -20,6 +20,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -32,6 +33,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { MyConfig.class })
+@EnableConfigurationProperties
 public class MyConfigTests {
 
 	@Autowired
